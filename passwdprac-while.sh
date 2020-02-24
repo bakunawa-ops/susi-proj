273e36fde343f8f8e2963f9153f6fa130854f153
bakunawa-ops <jong.pairez@gmail.com>
2020-02-24 20:10:56 +0900

    First Commit

---
+++ passwdprac-while.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Author: s3cr3cy.is.m49ic
+# While loop exercise
+# This script validates password if correctly written.
+
+Pass1='Put-your-first-pass-here'
+
+Pass2='Put-your-second-pass-here'
+
+# Pass1
+read -p "Type Personal Password and hit [ENTER]: " Personal
+
+while [ $Pass1 != $Personal ];
+	do 
+	read -p "Wrong! Type again your Personal Password: " Personal
+	done
+
+echo "Congratulations! You typed it correctly. Type next password."
+sleep 3
+
+# Pass2
+read -p "Type Work Password and hit [ENTER]: " Work
+
+while [ $Work != $Pass2 ];
+	do
+	read -p "Wrong! Type again your Work Password: " Work
+	done
+
+echo "You typed it correctly. Congratulations!"
+sleep 3
+
+
